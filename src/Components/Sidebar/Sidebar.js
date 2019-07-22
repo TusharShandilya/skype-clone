426466dc367e4ac313c493f8b704702cdceee4c3
Tushar Shandilya <tusharshandilya96@gmail.com>
2019-07-22 20:16:32 +0530

    basic structure made, addedd css to sidebar and main

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Sidebar.css';
 
 const Sidebar = () => {
-  return <aside className="sidebar">Sidebar</aside>;
+  return <aside className="Sidebar">Sidebar</aside>;
 };
 
 export default Sidebar;
