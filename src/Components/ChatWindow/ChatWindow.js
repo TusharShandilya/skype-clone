79a97b3b6d76a87c49406779c50a8b616929aa1d
Tushar Shandilya <tusharshandilya96@gmail.com>
2019-07-24 21:36:28 +0530

    added header component and its styling to main window. It show the active user and their status

--- src/Components/ChatWindow/ChatWindow.js
+++ src/Components/ChatWindow/ChatWindow.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
+import store from '../../redux/store';
+import Header from '../Header/Header';
+
 import './ChatWindow';
 
-const ChatWindow = () => {
+const ChatWindow = ({ activeUserId }) => {
+
+  const state = store.getState();
+  const activeUser = state.contacts[activeUserId];
+
   return (
     <div className='Chat__Window'>
-      This is chat Window
+      <Header user={activeUser}/>
     </div>
   );
-}
+};
 
 export default ChatWindow;
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -8,7 +8,6 @@ const Sidebar = ({ contacts }) => {
   return (
     <aside className="Sidebar">
       {contacts.map(contact => {
-        console.log("contacts", contact);
         return (<User user={contact} key={contact.user_id} />)
         })
       }
